9066691c0339f9b2b0e8b7ca983532da51e82720
Antriksh Dhand <adha5655@uni.sydney.edu.au>
2024-07-30 17:07:50 +1000

    Update example script

--- example_rfi.py
+++ example_rfi.py
@@ -1,17 +1,27 @@
 import cv2 as cv
-import numpy as np
 import numpy.ma as ma
-import matplotlib.pyplot as plt
-
-spec = cv.imread("img/spec.png", cv.IMREAD_GRAYSCALE)
-spec_masked = ma.array(spec, mask=ma.nomask)
 
 from seek.mitigation import sum_threshold
 
-rfi_mask = sum_threshold.get_rfi_mask(tod=spec_masked, mask=sum_threshold.get_empty_mask(spec_masked.shape), suppress_dilation=True)
+try:
+    spec = cv.imread("img/spec.png", cv.IMREAD_GRAYSCALE)
+    if spec is None:
+        raise FileNotFoundError()
+except:
+    print("File not found. Try again.")
+    exit()
 
-# Visualize the RFI mask
-plt.imshow(rfi_mask, cmap='gray')
-plt.title('RFI Mask')
-plt.colorbar()
-plt.show()
+spec_masked = ma.array(spec, mask=ma.nomask)
+
+rfi_mask = sum_threshold.get_rfi_mask(
+    tod=spec_masked, 
+    mask=None,
+    chi_1=35000,
+    eta_i=[0.5],
+    #eta_i=[0.5, 0.55, 0.62, 0.75, 1],
+    normalize_standing_waves=True,
+    suppress_dilation=False,
+    plotting=True,
+    sm_kwargs=None,
+    di_kwargs=None
+)
